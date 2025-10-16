ea68dfb962e86126fc634d6614a23252e7e2b510
Ryley <rmcconke@uwaterloo.ca>
2025-10-16 10:21:52 -0400

    duct cases added

---
+++ data/DUCT/AR_10_Ret_180/dynamicCode/_d7060f9e45123c8284d0c3c9aa6e930141581de9/codeStreamTemplate.C
@@ -0,0 +1,73 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) YEAR OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Template for use with codeStream.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dictionary.H"
+#include "Ostream.H"
+#include "Pstream.H"
+#include "unitConversion.H"
+
+//{{{ begin codeInclude
+
+//}}} end codeInclude
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    void codeStream_d7060f9e45123c8284d0c3c9aa6e930141581de9
+    (
+        Ostream& os,
+        const dictionary& dict
+    )
+    {
+//{{{ begin code
+        #line 1 ""
+os << (2580*1.500000e-05/1.000000e-03);
+//}}} end code
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
--- data/extract_duct_cases.sh
+++ data/extract_duct_cases.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Copy initial conditions from common/0_Baseline to each case's 00Baseline folder
+cd Duct-raw/rans
 
 for case in AR_1_Ret_180 AR_1_Ret_360 AR_3_Ret_180 AR_3_Ret_360 AR_5_Ret_180 AR_7_Ret_180 AR_10_Ret_180 AR_14_Ret_180; do
     cp -rL common/0_Baseline/* "$case/00Baseline/beta11/0/"
@@ -19,8 +20,9 @@ for case in AR_1_Ret_180 AR_1_Ret_360 AR_3_Ret_180 AR_3_Ret_360 AR_5_Ret_180 AR_
     cp -L common/singleGraphDiag "$case/00Baseline/beta11/system/"
     cp -L common/wallValues "$case/00Baseline/beta11/system/"
     find "$case/00Baseline/beta11" -type f -size 0 -delete
-    mkdir -p "DUCT/$case"
-    cp -r "$case/00Baseline/beta11"/* "DUCT/$case/"
+    mkdir -p "../../DUCT/$case"
+    cp -r "$case/00Baseline/beta11"/* "../../DUCT/$case/"
 done
+
 
 echo "Initial conditions copied."
